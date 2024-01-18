ec38973168c89b87557c027efa2d1371727109e2
arun2521 <arunlee810@gmail.com>
2024-01-18 20:10:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Search from "./Components/search/search";
+import CurrentWeather from "./Components/current-weather/current-weather";
+import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
+import { useState } from "react";
+import ForeCast from "./Components/forecast/forecats";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
 function App() {
+  const [currentWeather, setcurrentWeather] = useState(null);
+  const [forecast, setForeCast] = useState(null);
+
+  const handleOnSearchChange = (searchData) => {
+    const [lat, lon] = searchData.value.split(" ");
+
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+
+    const forecastFetch = fetch(
+      `${WEATHER_API_URL}forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forecastResponse = await response[1].json();
+
+        setcurrentWeather({ city: searchData.label, ...weatherResponse });
+        setForeCast({ city: searchData.label, ...forecastResponse });
+      })
+      .catch((err) => console.log(err));
+  };
+
+  console.log(currentWeather);
+  console.log(forecast);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="card">
+        <Search onSearchChange={handleOnSearchChange} />
+        {forecast && (
+          <>
+            <ForeCast data={forecast} />
+            <ResponsiveContainer width="100%" height={300}>
+              <LineChart
+                data={forecast.list}
+                margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+              >
+                <XAxis dataKey="dt_txt" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line
+                  type="monotone"
+                  dataKey="main.temp"
+                  name="Temperature (°C)"
+                  stroke="#8884d8"
+                />
+              </LineChart>
+            </ResponsiveContainer>
+          </>
+        )}
+        {currentWeather && <CurrentWeather data={currentWeather} />}
+      </div>
     </div>
   );
 }
